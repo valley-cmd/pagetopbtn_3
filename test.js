3d4e4fbf8df4d5780a4f192a662a9078afe3edb6
valley nk <n.valley.k@gmail.com>
2020-10-16 17:43:53 +0900

    テンプレ

---
+++ test.js
@@ -0,0 +1,15 @@
+$(function(){
+  var pagetop = $('#pagetop');
+  // 100px スクロールしたらボタン表示
+  $(window).scroll(function () {
+     if ($(this).scrollTop() > 100) {
+          pagetop.fadeIn();
+     } else {
+          pagetop.fadeOut();
+     }
+  });
+  pagetop.click(function () {
+     $('body, html').animate({ scrollTop: 0 }, 500);
+     return false;
+  });
+});
